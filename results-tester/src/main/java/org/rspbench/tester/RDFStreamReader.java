502b73f65732cac7866bd4635c5d47b845ca6f84
Peter Wetz <peter.wetz@tuwien.ac.at>
2015-02-09 14:34:34 +0100

    fixed a bug in RDFStreamReader which avoided correct parsing of test output files

--- results-tester/src/main/java/org/rspbench/tester/RDFStreamReader.java
+++ results-tester/src/main/java/org/rspbench/tester/RDFStreamReader.java
@@ -12,6 +12,7 @@ import java.nio.file.Path;
 
 public class RDFStreamReader {
 
+	//s,p,o + timestamp/interval = 4
     private static final int TUPLE_SIZE = 4;
     private final BufferedReader reader;
     private String line;
@@ -25,12 +26,14 @@ public class RDFStreamReader {
     public boolean hasNext() throws IOException {
         line = reader.readLine();
         if (line != null) {
-            String[] tuple = line.split(" ", TUPLE_SIZE);
-            Resource subject = ResourceFactory.createResource(tuple[0]);
-            Property predicate = ResourceFactory.createProperty(tuple[1]);
-            RDFNode object = ResourceFactory.createTypedLiteral(tuple[2]);
+        	//+1 to avoid bug, if " ." is included in the last tuple prohibiting correct parsing of the time
+            String[] tuple = line.split(" ", TUPLE_SIZE+1);
+            System.out.println(tuple[3]);
+            Resource subject = ResourceFactory.createResource(tuple[0].substring(1, tuple[0].length()-1));
+            Property predicate = ResourceFactory.createProperty(tuple[1].substring(1, tuple[1].length()-1));
+            RDFNode object = ResourceFactory.createTypedLiteral(tuple[2].substring(1, tuple[2].length()-1));
             stmt = ResourceFactory.createStatement(subject, predicate, object);
-            time = Long.parseLong(tuple[TUPLE_SIZE - 1]);
+            time = Long.parseLong(tuple[TUPLE_SIZE - 1].substring(1, tuple[TUPLE_SIZE - 1].length()-1));
             return true;
         } else {
             stmt = null;
