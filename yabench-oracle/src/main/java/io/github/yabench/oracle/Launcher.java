174d6430a5776aea1e1499b8487e7148fd7d196e
Maxim Kolchin <kolchinmax@gmail.com>
2015-02-27 16:38:47 +0300

    Refactored the oracle's Launcher

--- yabench-oracle/src/main/java/io/github/yabench/oracle/Launcher.java
+++ yabench-oracle/src/main/java/io/github/yabench/oracle/Launcher.java
@@ -1,26 +1,32 @@
 package io.github.yabench.oracle;
 
+import io.github.yabench.commons.AbstractLauncher;
 import io.github.yabench.oracle.tests.TestFactory;
 import io.github.yabench.oracle.tests.OracleTest;
 import java.io.File;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
-public class Launcher {
+public class Launcher extends AbstractLauncher {
 
     private static final String PROGRAM_NAME = "yabench-oracle";
     private static final String ARG_HELP = "help";
     private static final String ARG_INPUTSTREAM = "inputstream";
     private static final String ARG_OUTPUTSTREAM = "outputstream";
     private static final String ARG_TESTNAME = "test";
-
+    
     public static void main(String[] args) {
+        Launcher launcher = new Launcher();
+        launcher.launch(args);
+    }
+
+    @Override
+    public void launch(String[] args) {
         CommandLineParser parser = new PosixParser();
         Options options = createCLIOptions();
 
@@ -35,7 +41,7 @@ public class Launcher {
             Option[] expectedOptions = testFactory
                     .getExpectedOptions(testName);
             if (expectedOptions != null) {
-                addOptions(options, expectedOptions);
+                mergeOptions(options, expectedOptions);
                 cli = parser.parse(options, args);
                 
                 if(cli.hasOption(ARG_HELP)) {
@@ -67,23 +73,6 @@ public class Launcher {
             ex.printStackTrace();
         }
     }
-    
-    private static Options addOptions(Options dest, Option... options) {
-        for(Option opt : options) {
-            dest.addOption(opt);
-        }
-        return dest;
-    }
-
-    private static void printHelp(Options options, String... messages) {
-        HelpFormatter formatter = new HelpFormatter();
-        System.out.println();
-        for (String msg : messages) {
-            System.out.println(msg);
-        }
-        System.out.println();
-        formatter.printHelp(PROGRAM_NAME, options);
-    }
 
     private static Options createCLIOptions() {
         Options opt = new Options();
@@ -114,6 +103,11 @@ public class Launcher {
         opt.addOption(testName);
         opt.addOption(help);
         return opt;
+    }
+
+    @Override
+    public String getName() {
+        return PROGRAM_NAME;
     }
 
 }
