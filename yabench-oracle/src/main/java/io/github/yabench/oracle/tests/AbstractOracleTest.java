68929fc513b96efb2b2791a156e511b232a58919
Maxim Kolchin <kolchinmax@gmail.com>
2015-03-02 19:16:25 +0300

    Initial version of the Q2

--- yabench-oracle/src/main/java/io/github/yabench/oracle/tests/AbstractOracleTest.java
+++ yabench-oracle/src/main/java/io/github/yabench/oracle/tests/AbstractOracleTest.java
@@ -1,26 +1,40 @@
 package io.github.yabench.oracle.tests;
 
+import io.github.yabench.oracle.BindingWindow;
+import io.github.yabench.oracle.FMeasure;
+import io.github.yabench.oracle.QueryExecutor;
 import io.github.yabench.oracle.ResultsReader;
+import io.github.yabench.oracle.TripleWindow;
+import io.github.yabench.oracle.WindowFactory;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.io.IOUtils;
 
 abstract class AbstractOracleTest implements OracleTest {
 
     private static final String QUERY_TEMPLATE_NAME = "query.template";
+    private static final String ARG_WINDOWSIZE = "windowsize";
+    private static final String ARG_WINDOWSLIDE = "windowslide";
     private final CommandLine cli;
     private final Reader inputStreamReader;
     private final Writer outputWriter;
     private final ResultsReader queryResultsReader;
+    private final Map<String, String> vars = new HashMap<>();
+    private long windowSize;
+    private long windowSlide;
+    private WindowFactory windowFactory;
 
-    AbstractOracleTest(File inputStream, File queryResults, File output, 
-            CommandLine cli) 
+    AbstractOracleTest(File inputStream, File queryResults, File output,
+            CommandLine cli)
             throws IOException {
         this.inputStreamReader = new FileReader(inputStream);
         this.outputWriter = new FileWriter(output);
@@ -28,20 +42,53 @@ abstract class AbstractOracleTest implements OracleTest {
         this.cli = cli;
     }
 
-    protected CommandLine getOptions() {
+    protected CommandLine getCommandLine() {
         return cli;
     }
 
     protected Reader getInputStreamReader() {
         return inputStreamReader;
     }
-    
+
     protected Writer getOutputWriter() {
         return outputWriter;
     }
-    
+
     protected ResultsReader getQueryResultsReader() {
         return queryResultsReader;
+    }
+
+    protected Map<String, String> getVars() {
+        return vars;
+    }
+
+    protected static Option[] getExpectedOptions() {
+        Option windowSize = OptionBuilder
+                .isRequired()
+                .withType(Long.class)
+                .hasArg()
+                .withArgName("ms")
+                .withDescription("the window size")
+                .create(ARG_WINDOWSIZE);
+
+        Option windowSlide = OptionBuilder
+                .isRequired()
+                .withType(Long.class)
+                .hasArg()
+                .withArgName("ms")
+                .withDescription("the window slide")
+                .create(ARG_WINDOWSLIDE);
+        return new Option[]{windowSize, windowSlide};
+    }
+
+    @Override
+    public void init() throws Exception {
+        windowSize = Long.parseLong(getCommandLine().getOptionValue(ARG_WINDOWSIZE));
+        windowSlide = Long.parseLong(getCommandLine().getOptionValue(ARG_WINDOWSLIDE));
+
+        windowFactory = new WindowFactory(getInputStreamReader(), windowSize, windowSlide);
+
+        getQueryResultsReader().initialize();
     }
 
     @Override
@@ -58,14 +105,40 @@ abstract class AbstractOracleTest implements OracleTest {
                 .append(QUERY_TEMPLATE_NAME).toString();
         return IOUtils.toString(this.getClass().getResourceAsStream(path));
     }
-    
-    protected String resolveVars(final String template, 
+
+    protected String resolveVars(final String template,
             final Map<String, String> vars) {
         String result = new String(template);
-        for(String key : vars.keySet()) {
+        for (String key : vars.keySet()) {
             result = result.replaceAll(key, vars.get(key));
         }
         return result;
+    }
+
+    @Override
+    public void compare() throws IOException {
+        final QueryExecutor qexec = new QueryExecutor();
+        final String query = resolveVars(loadQueryTemplate(), vars);
+        TripleWindow inputWindow;
+        while ((inputWindow = windowFactory.nextWindow()) != null) {
+            final BindingWindow expected = qexec.executeSelect(
+                    inputWindow, query);
+            final BindingWindow actual = getQueryResultsReader().nextWindow();
+
+            FMeasure fMeasure = new FMeasure();
+            fMeasure.updateScores(expected.getBindings().toArray(),
+                    actual.getBindings().toArray());
+
+            getOutputWriter().write(
+                    new StringBuilder()
+                    .append(fMeasure.getPrecisionScore())
+                    .append('\t')
+                    .append(fMeasure.getRecallScore())
+                    .append('\t')
+                    .append(actual.getEnd() - expected.getEnd())
+                    .append('\n')
+                    .toString());
+        }
     }
 
 }
--- yabench-oracle/src/main/java/io/github/yabench/oracle/tests/TestQ2.java
+++ yabench-oracle/src/main/java/io/github/yabench/oracle/tests/TestQ2.java
@@ -1,5 +1,6 @@
 package io.github.yabench.oracle.tests;
 
+import static io.github.yabench.oracle.tests.AbstractOracleTest.getExpectedOptions;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -10,27 +11,27 @@ import org.apache.commons.cli.OptionBuilder;
 
 /**
  * C-SPARQL query:
- *
- * PREFIX om-owl: <http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#>
- * PREFIX weather: <http://knoesis.wright.edu/ssw/ont/weather.owl#>
- *
- * REGISTER QUERY q AS SELECT ?sensor ?tempvalue ?obs 
- * FROM NAMED STREAM <http://cwi.nl/SRBench/observations> [RANGE %WSIZE% MS STEP %WSLIDE% MS]
+ * 
+ * PREFIX om-owl: <http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#> 
+ * PREFIX weather: <http://knoesis.wright.edu/ssw/ont/weather.owl#> 
+ * 
+ * REGISTER QUERY q AS SELECT (AVG(?value) AS ?avg)
+ * FROM NAMED STREAM <http://cwi.nl/SRBench/observations> [RANGE %WSIZE% S STEP %WSLIDE% S]
  * WHERE { 
- *  ?obs om-owl:observedProperty weather:_AirTemperature ;
- *      om-owl:procedure ?sensor ; 
- *      om-owl:result [om-owl:floatValue ?tempvalue] .
- *  FILTER(?tempvalue > %TEMP%)
+ *  ?obs om-owl:observedProperty weather:_AirTemperature ;  
+ *      om-owl:procedure ?sensor ;  
+ *      om-owl:result ?res .
+ *  ?res om-owl:floatValue ?value .
+ *  FILTER(?value > $TEMP$)
  * }
  */
-public class TestQ1 extends AbstractOracleTest {
+public class TestQ2 extends AbstractOracleTest {
 
     private static final String ARG_TEMPERATURE = "temp";
     private static final String VAR_KEY_TEMP = "%TEMP%";
 
-    public TestQ1(File inputStream, File queryResults, File output,
-            CommandLine cli)
-            throws IOException {
+    public TestQ2(File inputStream, File queryResults, File output,
+            CommandLine cli) throws IOException {
         super(inputStream, queryResults, output, cli);
     }
 
@@ -53,7 +54,14 @@ public class TestQ1 extends AbstractOracleTest {
 
     @Override
     public void init() throws Exception {
+        super.init();
         getVars().put(VAR_KEY_TEMP, 
                 getCommandLine().getOptionValue(ARG_TEMPERATURE));
     }
+
+    @Override
+    public void compare() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
 }
