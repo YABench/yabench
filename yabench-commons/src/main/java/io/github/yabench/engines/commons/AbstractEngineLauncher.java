59d09e7ac7060f900ccc1f07f64974cadd83ddf4
Peter Wetz <peter.wetz@tuwien.ac.at>
2015-05-21 14:33:34 +0200

    timing bugfix. see: http://stackoverflow.com/a/824472/973158 http://www.javamex.com/tutorials/threads/sleep_issues.shtml

--- yabench-commons/src/main/java/io/github/yabench/engines/commons/AbstractEngineLauncher.java
+++ yabench-commons/src/main/java/io/github/yabench/engines/commons/AbstractEngineLauncher.java
@@ -26,99 +26,123 @@ import org.slf4j.LoggerFactory;
 
 public abstract class AbstractEngineLauncher extends AbstractLauncher {
 
-    private static final Logger logger = LoggerFactory.getLogger(AbstractEngineLauncher.class);
+	private static final Logger logger = LoggerFactory.getLogger(AbstractEngineLauncher.class);
 
-    private static final String ARG_QUERY = "query";
-    private static final String ARG_SOURCE = "source";
-    private static final String ARG_DEST = "dest";
-    private static final String ARG_HELP = "help";
+	private static final String ARG_QUERY = "query";
+	private static final String ARG_SOURCE = "source";
+	private static final String ARG_DEST = "dest";
+	private static final String ARG_HELP = "help";
 
-    private Query query;
+	private Query query;
 
-    @Override
-    public abstract String getName();
+	@Override
+	public abstract String getName();
 
-    public Query getQuery() {
-        return query;
-    }
+	public Query getQuery() {
+		return query;
+	}
 
-    @Override
-    public void launch(String[] args) {
-        CommandLineParser parser = new PosixParser();
-        Options options = createCLIOptions();
-        try {
-            CommandLine cli = parser.parse(options, args);
+	@Override
+	public void launch(String[] args) {
+		CommandLineParser parser = new PosixParser();
+		Options options = createCLIOptions();
+		try {
+			CommandLine cli = parser.parse(options, args);
 
-            final String queryString = Files.toString(
-                    new File(cli.getOptionValue(ARG_QUERY)), Charset.defaultCharset());
-            final File source = new File(cli.getOptionValue(ARG_SOURCE));
-            final File dest = new File(cli.getOptionValue(ARG_DEST));
-            query = new QueryFactory().create(queryString);
+			final String queryString = Files.toString(new File(cli.getOptionValue(ARG_QUERY)), Charset.defaultCharset());
+			final File source = new File(cli.getOptionValue(ARG_SOURCE));
+			final File dest = new File(cli.getOptionValue(ARG_DEST));
+			query = new QueryFactory().create(queryString);
 
-            try (final TemporalRDFReader reader = new TemporalRDFReader(source);
-                    final Writer writer = new BufferedWriter(new FileWriter(dest))) {
-                logger.info("initialize engine");
-                EngineFactory engineFactory = new EngineFactory();
-                try (Engine engine = engineFactory.create()) {
-                    engine.initialize();
-                    final ResultSerializer serializer = new ResultSerializer(writer);
-                    TemporalGraph graph;
-                    long t1 = 0, t2 = 0, time = 0;
-                    engine.registerQuery(query, serializer);
-                    serializer.initialize();
-                    logger.info("started sending triples at {}", Instant.now());
-                    while ((graph = reader.readNextGraph()) != null) {
-                        t1 = (System.nanoTime() - t2) / 1000000;
-                        Thread.sleep(graph.getTime() - time - t1 < 0
-                                ? graph.getTime() - time
-                                : graph.getTime() - time - t1);
-                        t2 = System.nanoTime();
+			// set up daemon thread for higher sleep granularity
+			Thread t = new Thread(new DaemonThread());
+			t.setDaemon(true);
+			t.start();
 
-                        for (TemporalTriple triple : graph.getTriples()) {
-                            engine.stream(triple.getStatement());
-                        }
-                        time = graph.getTime();
-                    }
+			try (final TemporalRDFReader reader = new TemporalRDFReader(source);
+					final Writer writer = new BufferedWriter(new FileWriter(dest))) {
+				logger.info("initialize engine");
+				EngineFactory engineFactory = new EngineFactory();
+				try (Engine engine = engineFactory.create()) {
+					engine.initialize();
+					final ResultSerializer serializer = new ResultSerializer(writer);
+					TemporalGraph graph;
+					long t1 = 0, t2 = 0, time = 0, sleep = 0, starts = 0, sleeptime = 0;
+					engine.registerQuery(query, serializer);
+					serializer.initialize();
+					logger.info("started sending triples at {}", Instant.now());
+					while ((graph = reader.readNextGraph()) != null) {
+						t1 = (System.nanoTime() - t2) / 1000000;
+						sleep = graph.getTime() - time - t1 < 0 ? graph.getTime() - time : graph.getTime() - time - t1;
+						starts = System.nanoTime();
+						Thread.sleep(sleep);
+						sleeptime = (System.nanoTime() - starts) / 1000000;
+						t2 = System.nanoTime();
 
-                    logger.info("stopped sending triples at {}", Instant.now());
+						logger.info("streamts,currtime,sleep,t1,actsleep {},{},{},{},{}", graph.getTime(), System.currentTimeMillis(),
+								sleep, t1, sleeptime);
+						for (TemporalTriple triple : graph.getTriples()) {
+							engine.stream(triple.getStatement());
+						}
+						time = graph.getTime();
 
-                    onInputStreamEnd();
+					}
 
-                    logger.info("stopped the engine at {}", Instant.now());
-                }
-            }
-        } catch (ParseException ex) {
-            printHelp(options, ex.getMessage());
-        } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
-        }
-    }
+					logger.info("stopped sending triples at {}", Instant.now());
 
-    public void onInputStreamEnd() throws InterruptedException {
-        // Nothing by default
-    }
+					t.interrupt();
+					onInputStreamEnd();
 
-    private Options createCLIOptions() {
-        Options opts = new Options();
+					logger.info("stopped the engine at {}", Instant.now());
+				}
+			}
+		} catch (ParseException ex) {
+			printHelp(options, ex.getMessage());
+		} catch (Exception ex) {
+			logger.error(ex.getMessage(), ex);
+		}
+	}
 
-        Option query = OptionBuilder.withArgName("query")
-                .withDescription("destination file containing the query to be registered at the engine").hasArg().isRequired()
-                .create(ARG_QUERY);
+	public void onInputStreamEnd() throws InterruptedException {
+		// Nothing by default
+	}
 
-        Option dest = OptionBuilder.withArgName("dest").withDescription("destination folder of query results").hasArg().isRequired()
-                .create(ARG_DEST);
+	private Options createCLIOptions() {
+		Options opts = new Options();
 
-        Option source = OptionBuilder.withArgName("source").withDescription("source file containing the triples to be streamed").hasArg()
-                .isRequired().create(ARG_SOURCE);
+		Option query = OptionBuilder.withArgName("query")
+				.withDescription("destination file containing the query to be registered at the engine").hasArg().isRequired()
+				.create(ARG_QUERY);
 
-        Option help = OptionBuilder.withDescription("print this message").create(ARG_HELP);
+		Option dest = OptionBuilder.withArgName("dest").withDescription("destination folder of query results").hasArg().isRequired()
+				.create(ARG_DEST);
 
-        opts.addOption(query);
-        opts.addOption(source);
-        opts.addOption(dest);
-        opts.addOption(help);
+		Option source = OptionBuilder.withArgName("source").withDescription("source file containing the triples to be streamed").hasArg()
+				.isRequired().create(ARG_SOURCE);
 
-        return opts;
-    }
+		Option help = OptionBuilder.withDescription("print this message").create(ARG_HELP);
+
+		opts.addOption(query);
+		opts.addOption(source);
+		opts.addOption(dest);
+		opts.addOption(help);
+
+		return opts;
+	}
+
+	class DaemonThread implements Runnable {
+
+		public void run() {
+
+			try {
+				Thread.sleep(Integer.MAX_VALUE);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				
+			}
+
+		}
+
+	}
 
 }
