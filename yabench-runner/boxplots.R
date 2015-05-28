060f8f843b4551f6627df629e0bec93f290ed72c
Peter Wetz <peter.wetz@tuwien.ac.at>
2015-05-28 13:24:01 +0200

    renamed boxplot output filenames

--- yabench-runner/boxplots.R
+++ yabench-runner/boxplots.R
@@ -2,8 +2,8 @@ options(echo=TRUE) # if you want see commands in output file
 args <- commandArgs(trailingOnly = TRUE)
 csvNames=strsplit(args[1], ",")[[1]]
 outputDir=args[2]
+testname=args[3]
 setwd(outputDir) 
-print(outputDir)
 mylist <- lapply(csvNames, read.csv, header=F, sep=',', dec='.')
 #print(mylist)
 
@@ -23,7 +23,7 @@ precList <- split(allData[,1], allData$rowID)
 #print(precList)
 #boxplot likes a list
 #png("boxplots.png")
-pdf("PrecisionBoxplots.pdf")
+pdf(paste("BoxplotPrecision_",testname, ".pdf", sep=''))
 boxplot(precList,ylim=c(0,1),names = labelList, ylab = "Precision", main="Precision per Window",yaxs = "i")
 grid()
 dev.off()
@@ -31,14 +31,14 @@ dev.off()
 #RECALL
 recList <- split(allData[,2], allData$rowID)
 #print(recList)
-pdf("RecallBoxplots.pdf")
+pdf(paste("BoxplotRecall_", testname, ".pdf", sep=''))
 boxplot(recList,ylim=c(0,1),names = labelList, ylab = "Recall", main="Recall per Window", yaxs = "i")
 grid()
 dev.off()
 
 #DELAY
 delayList <- split(allData[,8], allData$rowID)
-pdf("DelayBoxplots.pdf")
+pdf(paste("BoxplotDelay_", testname, ".pdf", sep=''))
 ylim <- c(min(unlist(lapply(delayList,FUN=min))), max(unlist(lapply(delayList,FUN=max))))
 boxplot(delayList,ylim=ylim, names = labelList, ylab = "Delay (ms)", main="Delay per Window")
 grid()
